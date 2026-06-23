c66e17194e6a9d9ad9d4978f9c5fe7862d081d2d
Raúl Solano <rasoberdiel3@gmail.com>
2026-06-23 19:47:56 +0200

    refactor(navigation): remove news route and delete unused news pages

--- config/navigation.ts
+++ config/navigation.ts
@@ -3,7 +3,6 @@ import { ROUTES } from "./routes";
 export const navigationConfig = {
   mainNav: [
     { href: ROUTES.team, labelKey: "team" },
-    { href: ROUTES.news, labelKey: "news" },
     { href: ROUTES.coaching, labelKey: "coaching" },
     { href: ROUTES.store, labelKey: "store" },
     { href: ROUTES.aboutUs, labelKey: "about_us" },
